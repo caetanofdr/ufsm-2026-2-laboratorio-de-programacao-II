5878ff1d2f751f1c8f7924a4ba35185ef540c0b6
Caetano Facco da Rosa <208185544+caetanofdr@users.noreply.github.com>
2026-08-11 23:47:17 -0300

    inicio

---
+++ trabalhos/trabalho01/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct timespec crono;
+
+void configura_terminal()
+{
+    if (system("stty raw opost -echo min 0 time 1") != 0) {
+        perror("erro na execução de system(\"stty\")");
+        fprintf(stderr, "você tem o programa stty instalado?\n");
+        exit(1);
+    };
+    if (setvbuf(stdin, NULL, _IONBF, 0) != 0) {
+        perror("erro na execução de setvbuf()");
+        exit(1);
+    }
+}
+
+void normaliza_terminal()
+{
+    system("stty sane");
+}
+
+char lechar()
+{
+    fflush(stdout);
+    clearerr(stdin); // coloquei isso aqui pois sem nao roda no meu pc
+    char c;
+    if (fread(&c, 1, 1, stdin) == 1) return c;
+    return 0;
+}
+
+void crono_inicia(crono *c)
+{
+    clock_gettime(CLOCK_MONOTONIC, c);
+}
+
+double crono_parcial(crono *c)
+{
+    crono agora;
+    clock_gettime(CLOCK_MONOTONIC, &agora);
+    double segundos = agora.tv_sec - c->tv_sec;
+    double nanosegundos = agora.tv_nsec - c->tv_nsec;
+    return segundos + 1e-9 * nanosegundos;
+}
+
+int main()
+{
+    configura_terminal();
+
+    
+
+    normaliza_terminal();
+    return 0;
+}
