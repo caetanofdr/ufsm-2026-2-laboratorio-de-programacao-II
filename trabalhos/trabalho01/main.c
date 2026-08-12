f5d7709d9a97058d1b4e4aff6526c7d5c9d90253
Caetano Facco da Rosa <208185544+caetanofdr@users.noreply.github.com>
2026-08-12 19:12:49 -0300

    criacao de estruturas, defines e menu principal

--- trabalhos/trabalho01/main.c
+++ trabalhos/trabalho01/main.c
@@ -2,6 +2,45 @@
 #include <stdlib.h>
 #include <time.h>
 
+// defines
+
+// estados do programa
+
+#define ESTADO_MENU_PRICIPAL 0
+#define ESTADO_MENU_FIM_PARTIDA 1
+#define ESTADO_MENU_MELHORES_POTUACOES 2
+#define ESTADO_RODADA_DIURNA 3
+#define ESTADO_RODADA_NOTURNA 4
+#define ESTADO_FIM_PROGRAMA -1
+
+// elementos do campo
+
+#define ESCUDO -1
+#define VAZIO -2
+
+// estruturas
+
+// estrutura principal do jogo
+
+typedef struct  Partida
+{
+    unsigned char armaAtual;
+    unsigned char tipoOrda;
+    unsigned char quantidadeEscudos;
+    unsigned char municao;
+
+    unsigned int pontuacao;
+
+    int campoAtaquesDiurnos[13];
+    int campoAtaquesNoturnos[8];
+} Partida;
+
+typedef struct Programa
+{
+    char estado;
+    Partida partidaAtual;
+} Programa;
+
 typedef struct timespec crono;
 
 void configura_terminal()
@@ -45,12 +84,115 @@ double crono_parcial(crono *c)
     return segundos + 1e-9 * nanosegundos;
 }
 
-int main()
+void inicializarPrograma(Programa *programa) 
 {
     configura_terminal();
 
-    
+    programa->estado = ESTADO_MENU_PRICIPAL;
+}
 
+void finalizarPrograma(Programa *programa)
+{
     normaliza_terminal();
+}
+
+void desenharMenuPrincipal()
+{
+    system("clear");
+    printf("Bem-vindo ao Invasores do Espaço!\n");
+    printf("1 - Jogar nova partida\n");
+    printf("2 - Placar das melhores pontuações\n");
+    printf("3 - Sair do Invasozer do Espaço\n");
+
+    printf("Sua escolha: ");
+}
+
+void menuPrincipal(Programa *programa)
+{
+    char buffer[2];
+    int tam = 0;
+    buffer[0] = '\0';
+
+    desenharMenuPrincipal();
+
+    for (;;)
+    {
+        char c = lechar();
+        if (c == 0) continue;
+
+        if (c == '\r' || c == '\n')
+        {
+            break;
+        } else if (c == 127 || c == 8)
+        {
+            if (tam > 0)
+            {
+                tam--;
+                buffer[tam] = '\0';
+                printf("\b \b");
+            }
+        } else if (tam < 1)
+        {
+            buffer[tam] = c;
+            tam++;
+            buffer[tam] = '\0';
+            putchar(c);
+        }
+    }
+
+    int escolha = atoi(buffer);
+
+    switch (escolha)
+    {
+    case 1:
+        programa->estado = ESTADO_RODADA_DIURNA;
+        break;
+    case 2:
+        programa->estado = ESTADO_MENU_MELHORES_POTUACOES;
+        break;
+    case 3:
+        programa->estado = ESTADO_FIM_PROGRAMA;
+        break;
+    default:
+        programa->estado = ESTADO_MENU_PRICIPAL;
+        break;
+    }
+}
+
+int main()
+{
+    Programa programa;
+    inicializarPrograma(&programa);
+    
+    do
+    {
+        switch (programa.estado)
+        {
+        case ESTADO_MENU_PRICIPAL:
+            menuPrincipal(&programa);
+            break;
+        
+        case ESTADO_MENU_FIM_PARTIDA:
+            
+            break;
+
+        case ESTADO_MENU_MELHORES_POTUACOES:
+
+            break;
+
+        case ESTADO_RODADA_DIURNA:
+
+            break;
+
+        case ESTADO_RODADA_NOTURNA:
+
+            break;
+        default:
+            programa.estado = 0;
+            break;
+        }
+    } while (programa.estado != ESTADO_FIM_PROGRAMA);
+    
+    finalizarPrograma(&programa);
     return 0;
 }
