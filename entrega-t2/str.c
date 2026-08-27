6185575e9118a53d5ffe478bcad6a8a7a28f91db
Caetano Facco da Rosa <208185544+caetanofdr@users.noreply.github.com>
2026-08-26 23:17:54 -0300

    trabalho 2 pronto (eu acho)

--- entrega-t2/str.c
+++ entrega-t2/str.c
@@ -54,10 +54,10 @@ static void s_ok(Str_c s)
 
     int ncaracteres_reais = u8_conta_unichar_nos_bytes(s->nbytes, s->dados);
     assert(ncaracteres_reais != -1);
-    assert(ncaracteres_reais == s->ncaracteres);
+    assert(ncaracteres_reais == (int) s->ncaracteres);
 }
 
-static unsigned int s_proxima_potencia_2(int n)
+static unsigned int s_proxima_potencia_2(unsigned int n)
 {
     unsigned int p = MIN_ALLOC;
 
@@ -88,7 +88,7 @@ static void s_define_conteudo(Str s, byte const *conteudo, int nbytes)
     s->ncaracteres = ncaracteres;
 }
 
-static int s_posiscao_corrigida(int posicao, int n)
+static int s_posicao_corrigida(int posicao, int n)
 {
     if (posicao < 0) {
         posicao = n + posicao + 1;
@@ -114,6 +114,44 @@ static bool s_caractere_pertence(unichar c, Str_c sb)
     }
 
     return false;
+}
+
+static void s_garante_memoria(Str s, unsigned int nbytes_necessarios)
+{
+    if (nbytes_necessarios == 0) {
+        free(s->dados);
+        s->dados = NULL;
+        s->nmemoria = 0;
+        return;
+    }
+
+    bool precisa_realocar = (s->nmemoria < nbytes_necessarios) ||
+        (s->nmemoria > MIN_ALLOC && s->nmemoria > 3 * nbytes_necessarios);
+
+    if (precisa_realocar) {
+        unsigned int nova_memoria = s_proxima_potencia_2(nbytes_necessarios);
+        byte *novo_dados = realloc(s->dados, nova_memoria);
+        assert(novo_dados != NULL);
+        s->dados = novo_dados;
+        s->nmemoria = nova_memoria;
+    }
+}
+
+static void s_intervalo_corrigido(int pos, int tam, int n, int *pos_final, int *tam_final)
+{
+    long long inicio = (pos < 0) ? n + pos + 1 : pos;
+    long long fim = (tam < 0) ? n : inicio + tam;
+
+    if (inicio < 0) inicio = 0;
+    if (inicio > n) inicio = n;
+
+    if (fim < 0) fim = 0;
+    if (fim > n) fim = n;
+
+    if (fim < inicio) fim = inicio;
+
+    *pos_final = inicio;
+    *tam_final = fim - inicio;
 }
 
 // operações de criação e destruição {{{1
@@ -172,15 +210,15 @@ Str s_cria_de_arquivo(char *nome)
     fseek(arquivo, 0, SEEK_SET);
 
     if (tamamanho > 0) {
-        byte *tmp = malloc(tamamanho);
-        assert(tmp != NULL);
-        size_t lidos = fread(tmp, 1, tamamanho, arquivo);
+        byte *buffer_arquivo = malloc(tamamanho);
+        assert(buffer_arquivo != NULL);
+        size_t lidos = fread(buffer_arquivo, 1, tamamanho, arquivo);
 
         if (lidos == (size_t)tamamanho) {
-            s_define_conteudo(s, tmp, tamamanho);
+            s_define_conteudo(s, buffer_arquivo, tamamanho);
         }
 
-        free(tmp);
+        free(buffer_arquivo);
     }
 
     fclose(arquivo);
@@ -200,16 +238,16 @@ char *s_strc(Str_c s)
 {
     s_ok(s);
 
-    char *c = malloc(s->nbytes + 1);
-    assert(c != NULL);
+    char *string_c = malloc(s->nbytes + 1);
+    assert(string_c != NULL);
 
     if (s->nbytes > 0) {
-        memcpy(c, s->dados, s->nbytes);
+        memcpy(string_c, s->dados, s->nbytes);
     }
 
-    c[s->nbytes] = '\0';
+    string_c[s->nbytes] = '\0';
 
-    return c;
+    return string_c;
 }
 
 unichar s_ch(Str_c s, int pos)
@@ -258,7 +296,9 @@ bool s_igual(Str_c s, Str_c sb)
         return true;
     }
 
-    return memcmp(s->dados, sb->dados, s->nbytes) == 0;
+    bool resposta = (memcmp(s->dados, sb->dados, s->nbytes) == 0);
+
+    return resposta;
 }
 
 int s_busca_c(Str_c s, int pos, Str_c sb)
@@ -284,10 +324,10 @@ int s_busca_nc(Str_c s, int pos, Str_c sb)
     s_ok(sb);
 
     int n = s->ncaracteres;
-    pos = s_pos_corrigida(pos, n);
+    pos = s_posicao_corrigida(pos, n);
 
     for (int i = pos; i < n; i++) {
-        if (!s_char_pertence(s_ch(s, i), sb)) {
+        if (!s_caractere_pertence(s_ch(s, i), sb)) {
             return i;
         }
     }
@@ -301,10 +341,10 @@ int s_busca_rc(Str_c s, int pos, Str_c sb)
     s_ok(sb);
 
     int n = s->ncaracteres;
-    pos = s_pos_corrigida(pos, n);
+    pos = s_posicao_corrigida(pos, n);
 
     for (int i = pos - 1; i >= 0; i--) {
-        if (s_char_pertence(s_ch(s, i), sb)) {
+        if (s_caractere_pertence(s_ch(s, i), sb)) {
             return i;
         }
     }
@@ -318,10 +358,10 @@ int s_busca_rnc(Str_c s, int pos, Str_c sb)
     s_ok(sb);
 
     int n = s->ncaracteres;
-    pos = s_pos_corrigida(pos, n);
+    pos = s_posicao_corrigida(pos, n);
 
     for (int i = pos - 1; i >= 0; i--) {
-        if (!s_char_pertence(s_ch(s, i), sb)) {
+        if (!s_caractere_pertence(s_ch(s, i), sb)) {
             return i;
         }
     }
@@ -336,7 +376,7 @@ int s_busca_s(Str_c s, int pos, Str_c buscada)
 
     int n = s->ncaracteres;
     int m = buscada->ncaracteres;
-    pos = s_pos_corrigida(pos, n);
+    pos = s_posicao_corrigida(pos, n);
 
     if (m == 0) {
         return pos;
@@ -350,11 +390,12 @@ int s_busca_s(Str_c s, int pos, Str_c buscada)
                 break;
             }
         }
+
         if (igual) {
             return i;
         }
     }
-    
+
     return -1;
 }
 
@@ -363,15 +404,104 @@ int s_busca_s(Str_c s, int pos, Str_c buscada)
 void s_substitui(Str s, int pos, int tam, Str_c sb)
 {
     s_ok(s);
-    s_ok(sb);
-    //...
+
+    Str copia_temp = NULL;
+    if (sb != NULL) {
+        s_ok(sb);
+        if (sb == (Str_c) s) {
+            copia_temp = s_cria_cópia(sb);
+            sb = copia_temp;
+        }
+    }
+
+    int n = s->ncaracteres;
+    s_intervalo_corrigido(pos, tam, n, &pos, &tam);
+
+    byte *inicio_remocao = u8_avanca_unichar(s->dados, pos);
+    byte *fim_remocao = u8_avanca_unichar(s->dados, pos + tam);
+
+    int bytes_antes = inicio_remocao - s->dados;
+    int bytes_removidos = fim_remocao - inicio_remocao;
+    int bytes_depois = s->nbytes - bytes_antes - bytes_removidos;
+
+    int nbytes_sb = (sb != NULL) ? sb->nbytes : 0;
+    int ncaracteres_sb = (sb != NULL) ? sb->ncaracteres : 0;
+    int novo_nbytes = s->nbytes - bytes_removidos + nbytes_sb;
+
+    if (novo_nbytes == 0) {
+        free(s->dados);
+        s->dados = NULL;
+        s->nmemoria = 0;
+        s->nbytes = 0;
+        s->ncaracteres = 0;
+        if (copia_temp != NULL) s_destroi(copia_temp);
+        s_ok(s);
+        return;
+    }
+
+    byte *depois = NULL;
+    if (bytes_depois > 0) {
+        depois = malloc(bytes_depois);
+        assert(depois != NULL);
+        memcpy(depois, s->dados + bytes_antes + bytes_removidos, bytes_depois);
+    }
+
+    s_garante_memoria(s, novo_nbytes);
+
+    if (nbytes_sb > 0) {
+        memmove(s->dados + bytes_antes, sb->dados, nbytes_sb);
+    }
+
+    if (bytes_depois > 0) {
+        memmove(s->dados + bytes_antes + nbytes_sb, depois, bytes_depois);
+        free(depois);
+    }
+
+    s->nbytes = novo_nbytes;
+    s->ncaracteres = n - tam + ncaracteres_sb;
+
+    if (copia_temp != NULL) s_destroi(copia_temp);
+
+    s_ok(s);
 }
 
 void s_substring(Str s, Str_c sb, int pos, int tam)
 {
     s_ok(s);
     s_ok(sb);
-    //...
+
+    Str copia_temp = NULL;
+    if (sb == (Str_c) s) {
+        copia_temp = s_cria_cópia(sb);
+        sb = copia_temp;
+    }
+
+    int m = sb->ncaracteres;
+    s_intervalo_corrigido(pos, tam, m, &pos, &tam);
+
+    byte *inicio = u8_avanca_unichar(sb->dados, pos);
+    byte *fim = u8_avanca_unichar(sb->dados, pos + tam);
+    int nbytes = fim - inicio;
+
+    byte *trecho = NULL;
+    if (nbytes > 0) {
+        trecho = malloc(nbytes);
+        assert(trecho != NULL);
+        memcpy(trecho, inicio, nbytes);
+    }
+
+    free(s->dados);
+    s->dados = NULL;
+    s->nbytes = 0;
+    s->ncaracteres = 0;
+    s->nmemoria = 0;
+
+    s_define_conteudo(s, trecho, nbytes);
+    free(trecho);
+
+    if (copia_temp != NULL) s_destroi(copia_temp);
+
+    s_ok(s);
 }
 
 void s_copia(Str s, Str_c sb)
@@ -387,7 +517,17 @@ void s_insere(Str s, int pos, Str_c sb)
 void s_insere_c(Str s, int pos, unichar c)
 {
     s_ok(s);
-    //...
+
+    byte buffer_utf8[4];
+    int nbytes = u8_converte_pra_utf8(c, buffer_utf8);
+    if (nbytes == -1) {
+        return;
+    }
+
+    Str temp = s_cria("");
+    s_define_conteudo(temp, buffer_utf8, nbytes);
+    s_substitui(s, pos, 0, temp);
+    s_destroi(temp);
 }
 
 void s_anexa(Str s, Str_c sb)
@@ -409,7 +549,23 @@ void s_apara(Str s, Str_c sobras)
 {
     s_ok(s);
     s_ok(sobras);
-    //...
+
+    int n = s->ncaracteres;
+
+    int fim = n;
+    while (fim > 0 && s_caractere_pertence(s_ch(s, fim - 1), sobras)) {
+        fim--;
+    }
+
+    int inicio = 0;
+    while (inicio < fim && s_caractere_pertence(s_ch(s, inicio), sobras)) {
+        inicio++;
+    }
+
+    s_substitui(s, fim, n - fim, NULL);
+    s_substitui(s, 0, inicio, NULL);
+
+    s_ok(s);
 }
 
 // operações de E/S {{{1
@@ -417,13 +573,26 @@ void s_apara(Str s, Str_c sobras)
 void s_imprime(Str_c s)
 {
     s_ok(s);
-    //...
+
+    if (s->nbytes > 0) {
+        fwrite(s->dados, 1, s->nbytes, stdout);
+    }
 }
 
 void s_grava_arquivo(Str_c s, char *nome)
 {
     s_ok(s);
-    //...
+
+    FILE *arquivo = fopen(nome, "wb");
+    if (arquivo == NULL) {
+        return;
+    }
+
+    if (s->nbytes > 0) {
+        fwrite(s->dados, 1, s->nbytes, arquivo);
+    }
+
+    fclose(arquivo);
 }
 
 // vim: foldmethod=marker shiftwidth=2
--- entrega-t2/teste_str.c
+++ entrega-t2/teste_str.c
@@ -4,24 +4,319 @@
 
 #include "str.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 
-int main()
+static void testa_criacao_e_acesso(void)
 {
-  char *a = "Rábica";
-  Str s = s_cria(a);
-  printf("Deve escrever [%s] ", a);
-  s_imprime(s);
-  Str ss = s_cria_substring(s, 3, 2);
-  printf("\nDeve escrever [ic] ");
-  s_imprime(ss);
-  s_substring(ss, s, 1, 2);
-  printf("\nDeve escrever [áb] ");
-  s_imprime(ss);
-  if (s_tam(ss) != 2) printf("\ntamanho de 'áb' (%d) deveria ser 2\n", s_tam(ss));
-  s_substitui(ss, -2, 1, NULL);
-  s_substitui(s, -7, 3, ss);
-  printf("Deve escrever [áica] ");
-  s_imprime(s);
-  s_destroi(ss);
-  s_destroi(s);
+    Str s_vazia = s_cria("");
+    assert(s_tam(s_vazia) == 0);
+    assert(s_ch(s_vazia, 0) == UNI_INV);
+    assert(s_ch(s_vazia, -1) == UNI_INV);
+    char *c_vazia = s_strc(s_vazia);
+    assert(strcmp(c_vazia, "") == 0);
+    free(c_vazia);
+    s_destroi(s_vazia);
+
+    Str s_nula = s_cria(NULL);
+    assert(s_tam(s_nula) == 0);
+    s_destroi(s_nula);
+
+    Str s_utf8 = s_cria("Rábica € 𝄞 😀");
+    assert(s_tam(s_utf8) == 12);
+    assert(s_ch(s_utf8, 0) == 'R');
+    assert(s_ch(s_utf8, 1) == 0x00E1);
+    assert(s_ch(s_utf8, 2) == 'b');
+    assert(s_ch(s_utf8, -2) == 0x1F600);
+    assert(s_ch(s_utf8, -3) == ' ');
+    assert(s_ch(s_utf8, -4) == 0x1D11E);
+    assert(s_ch(s_utf8, 50) == UNI_INV);
+
+    char *c_utf8 = s_strc(s_utf8);
+    assert(strcmp(c_utf8, "Rábica € 𝄞 😀") == 0);
+    free(c_utf8);
+
+    Str copia = s_cria_cópia(s_utf8);
+    assert(s_igual(copia, s_utf8));
+    s_destroi(copia);
+
+    Str sub = s_cria_substring(s_utf8, 0, 6);
+    char *c_sub = s_strc(sub);
+    assert(strcmp(c_sub, "Rábica") == 0);
+    free(c_sub);
+    s_destroi(sub);
+
+    s_destroi(s_utf8);
+}
+
+static void testa_comparacao(void)
+{
+    Str s1 = s_cria("Programação");
+    Str s2 = s_cria("Programação");
+    Str s3 = s_cria("Programa");
+    Str s4 = s_cria("");
+    Str s5 = s_cria("");
+
+    assert(s_igual(s1, s2));
+    assert(!s_igual(s1, s3));
+    assert(!s_igual(s1, s4));
+    assert(s_igual(s4, s5));
+
+    s_destroi(s1);
+    s_destroi(s2);
+    s_destroi(s3);
+    s_destroi(s4);
+    s_destroi(s5);
+}
+
+static void testa_buscas(void)
+{
+    Str texto = s_cria("banana maçã");
+    Str conjunto = s_cria("aã");
+    Str nao_espaco = s_cria(" ");
+    Str sub = s_cria("na");
+    Str vazia = s_cria("");
+
+    assert(s_busca_c(texto, 0, conjunto) == 1);
+    assert(s_busca_c(texto, 2, conjunto) == 3);
+    assert(s_busca_c(texto, 8, conjunto) == 8);
+    assert(s_busca_c(texto, -2, conjunto) == 10);
+
+    assert(s_busca_nc(texto, 0, conjunto) == 0);
+    assert(s_busca_nc(texto, 1, nao_espaco) == 1);
+    assert(s_busca_nc(texto, 6, nao_espaco) == 7);
+
+    assert(s_busca_rc(texto, 0, conjunto) == -1);
+    assert(s_busca_rc(texto, 4, conjunto) == 3);
+    assert(s_busca_rc(texto, -1, conjunto) == 10);
+
+    assert(s_busca_rnc(texto, 2, conjunto) == 0);
+    assert(s_busca_rnc(texto, 7, nao_espaco) == 5);
+
+    assert(s_busca_s(texto, 0, sub) == 2);
+    assert(s_busca_s(texto, 3, sub) == 4);
+    assert(s_busca_s(texto, 5, sub) == -1);
+    assert(s_busca_s(texto, 0, vazia) == 0);
+    assert(s_busca_s(texto, 3, vazia) == 3);
+    assert(s_busca_s(texto, -1, vazia) == 11);
+
+    s_destroi(texto);
+    s_destroi(conjunto);
+    s_destroi(nao_espaco);
+    s_destroi(sub);
+    s_destroi(vazia);
+}
+
+static void testa_substituicao_e_remocao(void)
+{
+    Str s = s_cria("abácaxi");
+    Str rep1 = s_cria("te");
+    s_substitui(s, 5, 2, rep1);
+    char *c1 = s_strc(s);
+    assert(strcmp(c1, "abácate") == 0);
+    free(c1);
+    s_destroi(rep1);
+
+    Str ponto = s_cria(".");
+    s_substitui(s, 7, 0, ponto);
+    char *c2 = s_strc(s);
+    assert(strcmp(c2, "abácate.") == 0);
+    free(c2);
+
+    s_substitui(s, -1, 5, ponto);
+    char *c3 = s_strc(s);
+    assert(strcmp(c3, "abácate..") == 0);
+    free(c3);
+    s_destroi(ponto);
+
+    s_remove(s, -3, 2);
+    char *c4 = s_strc(s);
+    assert(strcmp(c4, "abácate") == 0);
+    free(c4);
+
+    Str s_num = s_cria("abácaxi");
+    Str ins = s_cria("123");
+    s_substitui(s_num, -4, 0, ins);
+    char *c5 = s_strc(s_num);
+    assert(strcmp(c5, "abác123axi") == 0);
+    free(c5);
+
+    s_copia(s_num, s);
+    s_substitui(s_num, -4, 1, ins);
+    char *c6 = s_strc(s_num);
+    assert(strcmp(c6, "abác123te") == 0);
+    free(c6);
+
+    s_copia(s_num, s);
+    s_substitui(s_num, 0, 1, ins);
+    char *c7 = s_strc(s_num);
+    assert(strcmp(c7, "123bácate") == 0);
+    free(c7);
+
+    s_copia(s_num, s);
+    s_substitui(s_num, -9, 1, ins);
+    char *c8 = s_strc(s_num);
+    assert(strcmp(c8, "123abácate") == 0);
+    free(c8);
+
+    s_substitui(s_num, 2, 200, NULL);
+    char *c9 = s_strc(s_num);
+    assert(strcmp(c9, "12") == 0);
+    free(c9);
+
+    s_destroi(s_num);
+    s_destroi(ins);
+    s_destroi(s);
+}
+
+static void testa_substring(void)
+{
+    Str base = s_cria("bárcó");
+    Str s = s_cria("");
+
+    s_substring(s, base, 2, 2);
+    char *c1 = s_strc(s);
+    assert(strcmp(c1, "rc") == 0);
+    free(c1);
+
+    s_substring(s, base, 2, 4);
+    char *c2 = s_strc(s);
+    assert(strcmp(c2, "rcó") == 0);
+    free(c2);
+
+    s_substring(s, base, 2, -1);
+    char *c3 = s_strc(s);
+    assert(strcmp(c3, "rcó") == 0);
+    free(c3);
+
+    s_substring(s, base, -3, 1);
+    char *c4 = s_strc(s);
+    assert(strcmp(c4, "c") == 0);
+    free(c4);
+
+    s_substring(s, base, -7, 3);
+    char *c5 = s_strc(s);
+    assert(strcmp(c5, "bá") == 0);
+    free(c5);
+
+    s_substring(s, base, 10, 5);
+    char *c6 = s_strc(s);
+    assert(strcmp(c6, "") == 0);
+    free(c6);
+
+    s_destroi(s);
+    s_destroi(base);
+}
+
+static void testa_insercao_e_anexo(void)
+{
+    Str s = s_cria("mundo");
+    Str prefixo = s_cria("olá ");
+    s_insere(s, 0, prefixo);
+    char *c1 = s_strc(s);
+    assert(strcmp(c1, "olá mundo") == 0);
+    free(c1);
+
+    s_insere_c(s, 3, '!');
+    char *c2 = s_strc(s);
+    assert(strcmp(c2, "olá! mundo") == 0);
+    free(c2);
+
+    Str sufixo = s_cria(" fim");
+    s_anexa(s, sufixo);
+    char *c3 = s_strc(s);
+    assert(strcmp(c3, "olá! mundo fim") == 0);
+    free(c3);
+
+    s_anexa_c(s, 0x1F600);
+    assert(s_ch(s, -2) == 0x1F600);
+
+    s_destroi(s);
+    s_destroi(prefixo);
+    s_destroi(sufixo);
+}
+
+static void testa_apara(void)
+{
+    Str s1 = s_cria("teste 1");
+    Str lixo = s_cria(" .");
+    s_apara(s1, lixo);
+    char *c1 = s_strc(s1);
+    assert(strcmp(c1, "teste 1") == 0);
+    free(c1);
+
+    Str s2 = s_cria("... teste 2. .");
+    s_apara(s2, lixo);
+    char *c2 = s_strc(s2);
+    assert(strcmp(c2, "teste 2") == 0);
+    free(c2);
+
+    Str s3 = s_cria("....   ...");
+    s_apara(s3, lixo);
+    assert(s_tam(s3) == 0);
+
+    s_destroi(s1);
+    s_destroi(s2);
+    s_destroi(s3);
+    s_destroi(lixo);
+}
+
+static void testa_autoatribuicao(void)
+{
+    Str s = s_cria("repetir");
+    s_copia(s, s);
+    char *c1 = s_strc(s);
+    assert(strcmp(c1, "repetir") == 0);
+    free(c1);
+
+    s_substring(s, s, 2, 3);
+    char *c2 = s_strc(s);
+    assert(strcmp(c2, "pet") == 0);
+    free(c2);
+
+    s_substitui(s, 0, 0, s);
+    char *c3 = s_strc(s);
+    assert(strcmp(c3, "petpet") == 0);
+    free(c3);
+
+    s_destroi(s);
+}
+
+static void testa_arquivo(void)
+{
+    char const *nome = "teste_temp.txt";
+    Str original = s_cria("Gravação e Leitura UTF-8: áéíóú € 𝄞");
+    s_grava_arquivo(original, (char *)nome);
+
+    Str carregada = s_cria_de_arquivo((char *)nome);
+    assert(s_igual(original, carregada));
+
+    remove(nome);
+
+    Str inexistente = s_cria_de_arquivo("arquivo_que_nao_existe_12345.xyz");
+    assert(s_tam(inexistente) == 0);
+
+    s_destroi(original);
+    s_destroi(carregada);
+    s_destroi(inexistente);
+}
+
+int main(void)
+{
+    testa_criacao_e_acesso();
+    testa_comparacao();
+    testa_buscas();
+    testa_substituicao_e_remocao();
+    testa_substring();
+    testa_insercao_e_anexo();
+    testa_apara();
+    testa_autoatribuicao();
+    testa_arquivo();
+
+    Str ok = s_cria("Todos os testes passaram com sucesso!\n");
+    s_imprime(ok);
+    s_destroi(ok);
+
+    return 0;
 }
