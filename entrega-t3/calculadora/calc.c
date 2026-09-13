e39831cef36578f162f3c57a1a3e9486354b9157
Caetano Facco da Rosa <208185544+caetanofdr@users.noreply.github.com>
2026-09-12 22:54:38 -0300

    comeco da calc.c

---
+++ entrega-t3/calculadora/calc.c
@@ -0,0 +1,105 @@
+#include "calc.h"
+#include "str.h"
+#include "lista.h"
+#include "dicionario.h"
+
+/* OPERAÇÕES */
+
+typedef enum {
+    OPERA,
+    TERMINA,
+    EMPILHA,
+    DESCARTA,
+    ERRO_FALTA_ABRE,
+    ERRO_FALTA_FECHA
+} acao_t;
+
+typedef enum {
+    CAT_FIM,
+    CAT_VAZIA,
+    CAT_ADITIVO,
+    CAT_MULTIPLICATIVO,
+    CAT_POTENCIA,
+    CAT_ABRE,
+    CAT_FECHA,
+    CAT_OPERANDO,
+    CAT_ERRO
+} categoria_t;
+
+/* TABELA DE PRECEDÊNCIA */
+
+static const acao_t tabela[7][7] = {
+    [CAT_VAZIA] = {
+        [CAT_FIM]            = TERMINA,
+        [CAT_ADITIVO]        = EMPILHA,
+        [CAT_MULTIPLICATIVO] = EMPILHA,
+        [CAT_POTENCIA]       = EMPILHA,
+        [CAT_ABRE]           = EMPILHA,
+        [CAT_FECHA]          = ERRO_FALTA_ABRE,
+    },
+
+    [CAT_ADITIVO] = {
+        [CAT_FIM]            = OPERA,
+        [CAT_ADITIVO]        = OPERA,
+        [CAT_MULTIPLICATIVO] = EMPILHA,
+        [CAT_POTENCIA]       = EMPILHA,
+        [CAT_ABRE]           = EMPILHA,
+        [CAT_FECHA]          = OPERA,
+    },
+
+    [CAT_MULTIPLICATIVO] = {
+        [CAT_FIM]            = OPERA,
+        [CAT_ADITIVO]        = OPERA,
+        [CAT_MULTIPLICATIVO] = OPERA,
+        [CAT_POTENCIA]       = EMPILHA,
+        [CAT_ABRE]           = EMPILHA,
+        [CAT_FECHA]          = OPERA,
+    },
+
+    [CAT_POTENCIA] = {
+        [CAT_FIM]            = OPERA,
+        [CAT_ADITIVO]        = OPERA,
+        [CAT_MULTIPLICATIVO] = OPERA,
+        [CAT_POTENCIA]       = EMPILHA,
+        [CAT_ABRE]           = EMPILHA,
+        [CAT_FECHA]          = OPERA,
+    },
+
+    [CAT_ABRE] = {
+        [CAT_FIM]            = ERRO_FALTA_FECHA,
+        [CAT_ADITIVO]        = EMPILHA,
+        [CAT_MULTIPLICATIVO] = EMPILHA,
+        [CAT_POTENCIA]       = EMPILHA,
+        [CAT_ABRE]           = EMPILHA,
+        [CAT_FECHA]          = DESCARTA,
+    },
+};
+
+static bool eh_operando(unichar c)
+{
+    return (c >= '0' && c <= '9') ||
+       (c >= 'a' && c <= 'z') ||
+       (c >= 'A' && c <= 'Z') ||
+       c == '.' || c == '_' || c == '$';
+}
+
+static categoria_t classifica_token(Str token)
+{
+    unichar c = s_ch(token, 0);
+
+    if (c == '+' || c == '-') {
+        return CAT_ADITIVO;
+    } else if (c == '*' || c == '/') {
+        return CAT_MULTIPLICATIVO;
+    } else if (c == '^') {
+        return CAT_POTENCIA;
+    } else if (c == '(') {
+        return CAT_ABRE;
+    } else if (c == ')') {
+        return CAT_FECHA;
+    } else if (eh_operando(c)) {
+        return CAT_OPERANDO;
+    }
+    
+    return CAT_ERRO;
+}
